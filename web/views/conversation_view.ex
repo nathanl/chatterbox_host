e1831d1e40f58daa80c0ba924eaff694d32fcf24
Nathan Long <him@nathanmlong.com>
2016-10-11 14:47:50 -0400

    Finish separating view from application

--- web/views/conversation_view.ex
+++ web/views/conversation_view.ex
@@ -1,3 +1,7 @@
 defmodule Consult.ConversationView do
-  use ChatterboxHost.Web, :view
+  use Phoenix.View, root: "web/templates"
+  # Import convenience functions from controllers
+  import Phoenix.Controller, only: [get_csrf_token: 0, get_flash: 2, view_module: 1]
+  # Use all HTML functionality (forms, tags, etc)
+  use Phoenix.HTML
 end
